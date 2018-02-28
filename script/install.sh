cfe6c2deeabfd10593c9aef572b9d07fb138d1d4
Hermes Martinez <Hermes@Narwhal.local>
2018-02-28 13:07:58 +0100

    added a small installer

---
+++ script/install.sh
@@ -0,0 +1,6 @@
+ln -s ~/.config/dot/vimrc ~/.vimrc
+ln -s ~/.config/dot/vim ~/.vim
+ln -s ~/.config/dot/tmux.conf ~/.tmux.conf
+ln -s ~/.config/dot/tmux-powerlinerc ~/.tmux-powerlinerc
+ln -s ~/.config/dot/tmux -~/.tmux
+echo "source ~/.config/dot/zshrc" >> ~/.zshrc
